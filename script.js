358a0af08303f1ebe6b0c35b2eb143b8f5ffd3bd
webdev-raj <codewithsapnap@gmail.com>
2025-01-19 18:40:55 +0530

    incomplete delete method and total addition

--- script.js
+++ script.js
@@ -1,40 +1,60 @@
 window.addEventListener("DOMContentLoaded", function () {
-    getFormValue()
-})
-var mainArray = new Array();
+  getFormValue();
+});
+var mainArray = [];
 var totalPrice = 0;
-var currentIndex = 0
-const getFormValue = () => {
-    let description = document.querySelector("#description")
-    let amount = document.querySelector("#number")
-    let dateVal = document.querySelector("input[type='date']")
-    let subBtn = document.querySelector("#subBtn")
-    let formElem = document.querySelector("form")
-    let selected = document.querySelector("#cate")
-    description.addEventListener("change", (e) => {
-        description = String(e.target.value)
-    })
-    amount.addEventListener("change", (e) => {
-        amount = Number(e.target.value)
-    })
-    subBtn.addEventListener("click", (e) => {
-        e.preventDefault()
-        dynamicObject();
-        console.log(mainArray)
-        formElem.reset()
-    })
-    const dynamicObject = () => {
-        mainArray.push(new Object)
-        mainArray[mainArray.length - 1].description = description
-        mainArray[mainArray.length - 1].amount = amount
-        mainArray[mainArray.length - 1].select = selected.options[selected.selectedIndex].textContent
-        mainArray[mainArray.length - 1].date = dateVal.value
-        totolPrice()
-    }
-}
-const totolPrice = () => {
-    totalPrice = totalPrice + mainArray[currentIndex].amount;
-    currentIndex++;
-    console.log(totalPrice);
-}
 
+const getFormValue = () => {
+  let description = document.querySelector("#description");
+  let amount = document.querySelector("#number");
+  let dateVal = document.querySelector("input[type='date']");
+  let subBtn = document.querySelector("#subBtn");
+  let formElem = document.querySelector("form");
+  let selected = document.querySelector("#cate");
+  let descriptionVal = "No description"
+  let amountVal = 0;
+  description.addEventListener("change", (e) => {
+    descriptionVal = String(e.target.value);
+  });
+  amount.addEventListener("change", (e) => {
+    amountVal = Number(e.target.value);
+  });
+  subBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    dynamicObject(descriptionVal, amountVal, selected.options[selected.selectedIndex].textContent, dateVal.value);
+    displayALL();
+    deleteMethod()
+    formElem.reset();
+  });
+  const dynamicObject = (description, amount, category, date) => {
+    mainArray.push({
+      expense_description: description,
+      expense_amount: amount,
+      select_reason: category,
+      expense_date: date
+    })
+  };
+};
+
+const displayALL = () => {
+  let clutter = "";
+  let table = document.querySelector("#tableBody");
+  for (let i = 0; i < mainArray.length; i++) {
+    clutter += `<tr class="">
+                                <td class="border px-4 py-2 text-xl text-zinc-400">${mainArray[i].expense_date}</td>
+                                <td class="border px-4 py-2 text-xl text-zinc-400">${mainArray[i].expense_description}</td>
+                                <td class="border px-4 py-2 text-xl text-zinc-400">${mainArray[i].select_reason}</td>
+                                <td class="border px-4 py-2 text-xl text-zinc-400">${mainArray[i].expense_amount}</td>
+                                <td class="border py-2 text-center">
+                                    <button class="deleteBtn bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700">Delete</button>
+                                </td>
+                            </tr>`;
+  }
+  table.innerHTML = clutter;
+};
+// const deleteMethod = ()=>{
+//   let deleteBtn = document.querySelector(".deleteBtn")
+//   deleteBtn.addEventListener("click",()=>{
+//     alert("Delete")
+//   })
+// }
